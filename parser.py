62e38a44d24f3e9d90b9c55c2449a5d0e5d2bd0b
MarsherSusanin <susanincsi@gmail.com>
2018-04-29 20:03:36 +1000

    v 0.1

--- parser.py
+++ parser.py
@@ -6,14 +6,20 @@ def get_html(url):
     return response.read()
 
 def parse (html):
-    soup = BeautifulSoup(html,'html.parser')
-    tittle=soup.find('div', class_="movie-summary")
-    print (tittle.prettify())
+    soup = BeautifulSoup(html,'html.parser') #Превращаем сайт в html текст
+    title=soup.find_all('div', class_='itemevent__head__name') # Ищем все контейнеры div с классом имени фильма
+
+    names=title.find('a')[0:]
+    print(title)
+
+
+    #print (title)
+    #print (alltitles)
 
 
 def main ():
-    parse(get_html('http://cinemate.cc/movies'))
-    
+    parse(get_html('https://kino.mail.ru/cinema/selection/2675_15_festivalnih_filmov_kotorie_neskuchno_smotret/'))
+
 
 if __name__=='__main__':
     main()
