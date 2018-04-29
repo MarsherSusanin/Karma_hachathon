65bf79a3506a8537d0d9e0dd45ca211a61ed80ea
Ivan <susanincsi@gmail.com>
2018-04-29 12:24:03 +1000

    Update first step

--- parser.py
+++ parser.py
@@ -6,13 +6,14 @@ def get_html(url):
     return response.read()
 
 def parse (html):
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html,'html.parser')
     tittle=soup.find('p', class_="Title")
     print (tittle.prettify())
 
 
 def main ():
     parse(get_html('https://www.filmpro.ru/materials/selections/63250'))
+    
 
 if __name__=='__main__':
     main()
